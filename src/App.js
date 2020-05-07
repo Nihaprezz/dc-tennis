61bd008caa90b222b9622c404bb8899179c5a7e5
nihaprezz <serg13prez@outlook.com>
2020-05-07 18:31:08 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -10,19 +10,20 @@ function App() {
     latitude: 38.89543914794922, 
     longitude: -77.03128051757812,
     width: "100vw", 
-    height: "100vh",
+    height: "93vh",
     zoom: 11
   })
 
   const [currentCourt, setCurrentCourt] = useState(null)
 
-  const handleSelect = (court) => {
-    setCurrentCourt(court)
-  }
 
   return (
     <div className="App">
-      <h1>DC Tennis</h1>
+      <div className="header">
+        <h1>Washington DC Tennis Courts</h1>
+        <a href="https://opendata.dc.gov/datasets/tennis-court-sites/geoservice">Data</a>
+        <a href="https://github.com/Nihaprezz/dc-tennis">Github</a>
+      </div>
 
       <ReactMapGL {...viewport} mapboxApiAccessToken={apiKey}
       mapStyle="mapbox://styles/nihaprezz/ck9w5z0yu05111intcxb0x1v2"
@@ -34,7 +35,7 @@ function App() {
             latitude={court.geometry.y}
             longitude={court.geometry.x}>
 
-            <button className="marker-btn" onClick={() => handleSelect(court)}>
+            <button className="marker-btn" onClick={() => setCurrentCourt(court)}>
               <img src={tennisIcon} alt="Tennis Icon"/>
             </button>
 
@@ -46,14 +47,12 @@ function App() {
           <Popup 
           latitude={currentCourt.geometry.y}
           longitude={currentCourt.geometry.x}
-          onClose={() => {
-            setCurrentCourt(null);
-          }}>
+          onClose={() => setCurrentCourt(null)}>
             <div>
-              <p>{currentCourt.attributes.NAME}</p>
-              <p>{`Condition: ${currentCourt.attributes.CONDITION}`}</p>
+              <p style={{fontWeight: 'bold'}}>{currentCourt.attributes.NAME}</p>
+              <p>{`Address: ${currentCourt.attributes.ADDRESS}`}</p>
+              <p>{`Condition: ${currentCourt.attributes.CONDITION} / Courts: ${currentCourt.attributes.COURTS}`}</p>
               <p>{`Surface: ${currentCourt.attributes.SURFACE}`}</p>
-
             </div>
           </Popup>
         ): null}
