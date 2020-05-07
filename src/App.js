c6a3d48732845df2bc0505b97f44e91152277da6
nihaprezz <serg13prez@outlook.com>
2020-05-06 22:38:43 -0400

    added inital dc map lat and long

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMapGL from "react-map-gl"
+
+const apiKey = process.env.REACT_APP_MAPBOX_TOKEN 
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 38.89543914794922, 
+    longitude: -77.03128051757812,
+    width: "100vw", 
+    height: "100vh",
+    zoom: 10
+  })
+
   return (
     <div className="App">
       <h1>DC Tennis Locations</h1>
+
+      <ReactMapGL {...viewport} mapboxApiAccessToken={apiKey}
+      onViewportChange={(viewport) => setViewport(viewport)}>
+
+      </ReactMapGL>
     </div>
   );
 }
