5858084c27bfeae7a216429deb1e10ef5fe3aecb
nihaprezz <serg13prez@outlook.com>
2020-05-07 18:42:53 -0400

    Updates

---
+++ precache-manifest.572271ab563c3a82c1930a6d145eab82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f120689ff97d013bbaf1174dfe0d74",
+    "url": "/dc-tennis/index.html"
+  },
+  {
+    "revision": "dce481c7f4f12da6241a",
+    "url": "/dc-tennis/static/css/main.89a3cb01.chunk.css"
+  },
+  {
+    "revision": "d2ff526b10e9ef28dc48",
+    "url": "/dc-tennis/static/js/2.eaf3f496.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/dc-tennis/static/js/2.eaf3f496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce481c7f4f12da6241a",
+    "url": "/dc-tennis/static/js/main.4c046360.chunk.js"
+  },
+  {
+    "revision": "6b3ac781715fe197e3cc",
+    "url": "/dc-tennis/static/js/runtime-main.aee6a348.js"
+  },
+  {
+    "revision": "af9b144e6d6a024214113b5222f3e52b",
+    "url": "/dc-tennis/static/media/tennis.af9b144e.svg"
+  }
+]);
